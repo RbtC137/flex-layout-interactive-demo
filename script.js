31d6c62518105d34f39ef4611ffc2cbc539aa10d
RbtC137 <128915939+RbtC137@users.noreply.github.com>
2023-10-11 22:41:21 +0800

    1. Set data inside button attribute in html 2. Create functions to get data from elements and set style to display

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const addButton = document.querySelector('#add-block');
 const deldButton = document.querySelector('#del-block');
+const modButtons = document.querySelectorAll('.mod-button')
 const displayArea = document.querySelector('#display-area');
 
 
 addButton.addEventListener('click', ()=>{addBlocks();});
+deldButton.addEventListener('click', ()=>{delBlocks();});
+modButtons.forEach(addListenerToButton);
+
+
 
 function addBlocks() {
     const div = document.createElement("div");
@@ -12,13 +17,19 @@ function addBlocks() {
     displayArea.appendChild(div);
 }
 
-deldButton.addEventListener('click', ()=>{delBlocks();});
-
 function delBlocks() {
     displayArea.lastElementChild.remove();
 }
-
+//!!!
 function updateBlocksStyle(styleName) {
-    displayArea.classList.remove();
-    displayArea.classList.add(styleName);
+    displayArea.setAttribute('style',``);
+}
+
+function addListenerToButton(button){
+    button.addEventListener('click', () => {
+        console.log(button.id);
+        console.log(button.dataset.key);
+        displayArea.setAttribute('style',`${button.dataset.key}:${button.id};`);
+
+    })
 }
