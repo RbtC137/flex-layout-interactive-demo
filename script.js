0844c64f03ab6517c70c15a19e4b7bfa19a0d479
RbtC137 <128915939+RbtC137@users.noreply.github.com>
2023-10-16 08:33:54 +0800

    1.Add info message to the display area 2.Add serial num to div blocks 3.Adjust layout of control panel

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const addButton = document.querySelector('#add-block');
 const deldButton = document.querySelector('#del-block');
-const modButtons = document.querySelectorAll('.mod-button')
+const modButtons = document.querySelectorAll('.mod-button');
 const displayArea = document.querySelector('#display-area');
+const controlPanel = document.querySelector('#control-panel');
+let styleString = "";
+let divNo = 1;
 
 
 addButton.addEventListener('click', ()=>{addBlocks();});
 deldButton.addEventListener('click', ()=>{delBlocks();});
 modButtons.forEach(addListenerToButton);
 
+controlPanel.addEventListener("click",removeInfoMsg);
+
+
+function removeInfoMsg() {
+    displayArea.firstElementChild.remove();
+    controlPanel.removeEventListener("click",removeInfoMsg);
+}
+
 
 
 function addBlocks() {
     const div = document.createElement("div");
-    div.innerText = "(^-^)";
+    div.innerText = `( ${divNo} )`;
+    divNo++;
     div.classList.add("blocks");
     displayArea.appendChild(div);
 }
 
 function delBlocks() {
     displayArea.lastElementChild.remove();
+    if(divNo>1){
+        divNo--;
+    }
 }
 //!!!
 function updateBlocksStyle(styleName) {
@@ -27,9 +42,10 @@ function updateBlocksStyle(styleName) {
 
 function addListenerToButton(button){
     button.addEventListener('click', () => {
-        console.log(button.id);
-        console.log(button.dataset.key);
-        displayArea.setAttribute('style',`${button.dataset.key}:${button.id};`);
+        // console.log(button.id);
+        // console.log(button.dataset.key);
+        styleString += `${button.dataset.key}:${button.id};`;
+        displayArea.setAttribute('style',styleString);
 
     })
 }
